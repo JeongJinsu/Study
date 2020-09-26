275b05541459463534bc9d57348b97636e3eaacb
jin <jeongjinsu@outlook.com>
2020-09-26 16:48:20 +0900

    feat: 순수 jdbc 이용-통합 테스트

--- hello-spring/hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ hello-spring/hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,14 +1,9 @@
-package hello.hellospring.survice;
+package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
-import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -67,10 +62,6 @@ class MemberServiceTest {
             assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다...");
         }
         */
-        //then
-
-        Member findMember =memberService.findOne(saveId).get();
-        assertThat(member1.getName()).isEqualTo(findMember.getName());
 
 
     }
