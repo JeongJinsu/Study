a60ae7dda8dabdb7b30a1355b14567601b2ede08
jin <jeongjinsu@outlook.com>
2020-10-04 18:55:21 +0900

    feat: @애노테이션 기반의 컴포넌트 스캔 방법 등장

--- springapplicationcontext/src/main/java/me/whiteship/springapplicationcontext/repository/BookRepository.java
+++ springapplicationcontext/src/main/java/me/whiteship/springapplicationcontext/repository/BookRepository.java
@@ -1,4 +1,7 @@
 package me.whiteship.springapplicationcontext.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class BookRepository {
 }
--- springapplicationcontext/src/main/java/me/whiteship/springapplicationcontext/service/BookService.java
+++ springapplicationcontext/src/main/java/me/whiteship/springapplicationcontext/service/BookService.java
@@ -1,9 +1,13 @@
 package me.whiteship.springapplicationcontext.service;
 
 import me.whiteship.springapplicationcontext.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookService {
 
+    @Autowired
     public BookRepository bookRepository;
 
     public void setBookRepository(BookRepository bookRepository) {
