8c622574faaca34815d5cb6a8a25a46c4e500d30
jin <jeongjinsu@outlook.com>
2020-10-05 18:32:34 +0900

    feat: 펑션을 사용한 빈등록 방법 애플리케이션 구동시 @ComponentScan, @Configuration@Bean 에 비해 성능상의 이점은 있으나, 일일이 등록해야 하는 단점도 존재

--- demospring51/src/main/java/me/whiteship/demospring51/BookService.java
+++ demospring51/src/main/java/me/whiteship/demospring51/BookService.java
@@ -6,17 +6,16 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public class BookService {
-    /* 1. BookRepository에 해당되는 빈 하나만 주입받을때
+    // 1. BookRepository에 해당되는 빈 하나만 주입받을때
     @Autowired @Qualifier("myBookRepository")
     BookRepository bookRepository;
 
     public void printBookRepository(){
         System.out.println(bookRepository.getClass());
     }
-    */
 
+    /*
     // 2. BookRepository에 해당되는 빈 모두를 주입 받고 싶을때
     @Autowired
     List<BookRepository> bookRepositories;
@@ -24,4 +23,5 @@ public class BookService {
     public void printBookRepository() {
         this.bookRepositories.forEach(System.out::println);
     }
+    */
 }
--- demospring51/src/main/java/me/whiteship/demospring51/Demospring51Application.java
+++ demospring51/src/main/java/me/whiteship/demospring51/Demospring51Application.java
@@ -2,12 +2,24 @@ package me.whiteship.demospring51;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 
 @SpringBootApplication
 public class Demospring51Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Demospring51Application.class, args);
+        var app = new SpringApplication(Demospring51Application.class);
+        //앱 실행 전, 빈을 등록할 경우 (싱글톤으로 인스턴스화 되는 @ComponentScan, @Configuration@Bean 에 비해 성능상의 이점은 있으나, 일일이 등록해야하는 단점)
+        app.addInitializers(new ApplicationContextInitializer<GenericApplicationContext>() {
+            @Override
+            public void initialize(GenericApplicationContext ctx) {
+                ctx.registerBean(BookService.class);
+            }
+        });
+        //앱 실행
+        app.run(args);
     }
 
 }
