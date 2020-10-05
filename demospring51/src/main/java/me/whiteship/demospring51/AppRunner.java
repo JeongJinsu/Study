a15e9b0be815f20d477a982ceceb3aae99970876
jin <jeongjinsu@outlook.com>
2020-10-05 21:27:15 +0900

    feat: 빈의 스코프 / 싱글톤 빈이 프로토타입 빈을 참조(의존)하면, 의도한 바와 달리 프로토타입 빈이 싱글톤처럼 작동한다. 싱글톤 빈 자체가 만들어질때 프로토타입 빈이 주입되기 때문.

--- demospring51/src/main/java/me/whiteship/demospring51/AppRunner.java
+++ demospring51/src/main/java/me/whiteship/demospring51/AppRunner.java
@@ -3,20 +3,30 @@ package me.whiteship.demospring51;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    Single single;
-
-    @Autowired
-    Proto proto;
+    ApplicationContext ctx;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(proto);
-        System.out.println(single.getProto());
+        System.out.println("proto");
+        System.out.println(ctx.getBean(Proto.class)); //Proto@3078cac
+        System.out.println(ctx.getBean(Proto.class)); //Proto@f6de586
+        System.out.println(ctx.getBean(Proto.class)); //Proto@5f2bd6d9
+
+        System.out.println("single");
+        System.out.println(ctx.getBean(Single.class)); //Single@43d9f1a2
+        System.out.println(ctx.getBean(Single.class)); //Single@43d9f1a2
+        System.out.println(ctx.getBean(Single.class)); //Single@43d9f1a2
+
+        System.out.println("proto by single");
+        System.out.println(ctx.getBean(Single.class).getProto()); //Proto@23f86d8a
+        System.out.println(ctx.getBean(Single.class).getProto()); //Proto@23f86d8a
+        System.out.println(ctx.getBean(Single.class).getProto()); //Proto@23f86d8a
     }
 }
--- demospring51/src/main/java/me/whiteship/demospring51/Proto.java
+++ demospring51/src/main/java/me/whiteship/demospring51/Proto.java
@@ -1,7 +1,13 @@
 package me.whiteship.demospring51;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component @Scope("prototype")
 public class Proto {
+
+    @Autowired
+    Single single;
+
 }
