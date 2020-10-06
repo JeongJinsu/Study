4eee5ccea68d549bf1a3140fb8798d0e7b09afcb
jin <jeongjinsu@outlook.com>
2020-10-06 14:34:31 +0900

    feat: 프로파일 생성(@ComponentScan@Profile("..."))

--- demospring51/src/main/java/me/whiteship/demospring51/TestBookRepository.java
+++ demospring51/src/main/java/me/whiteship/demospring51/TestBookRepository.java
@@ -1,5 +1,10 @@
 package me.whiteship.demospring51;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Profile("test") //@componentScan 에서도 프로파일 생성 가능
 public class TestBookRepository implements BookRepository {
 
 }
