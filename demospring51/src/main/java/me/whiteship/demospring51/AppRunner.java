6df877a4d417377d38bd3be577dd2afab158b5f3
jin <jeongjinsu@outlook.com>
2020-10-06 20:59:27 +0900

    feat: i18n, 메세지 다국화 하는 방법

--- demospring51/src/main/java/me/whiteship/demospring51/AppRunner.java
+++ demospring51/src/main/java/me/whiteship/demospring51/AppRunner.java
@@ -1,31 +1,22 @@
 package me.whiteship.demospring51;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.core.env.Environment;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
+
+import java.util.Locale;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ApplicationContext ctx;
-
-    @Autowired
-    BookRepository bookRepository;
-
-    @Value("${app.name}") //스프링부트 에서 제공하는 프로퍼티 불러오는 방법
-    String appName;
+    MessageSource messageSource;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Environment environment = ctx.getEnvironment();
-        System.out.println(environment.getProperty("app.name")); //설정-Edit-JVM으로 설정한 프로퍼티: -Dapp.name="spring" 를 불러오는 방법
-        System.out.println(environment.getProperty("app.about")); //.properties 확장 설정파일을 통해 설정한 프로퍼티 를 불러오는 방법
-
-        System.out.println(appName); //스프링부트 에서 제공하는 프로퍼티 불러오는 방법
+        System.out.println(messageSource.getMessage("greeting", new String[]{"keesun1"}, Locale.KOREA));
+        System.out.println(messageSource.getMessage("greeting", new String[]{"keesun2"}, Locale.getDefault())); //운영체제가 한글이라 _ko_KR.properties 적용됨
     }
 }
--- demospring51/src/main/java/me/whiteship/demospring51/Demospring51Application.java
+++ demospring51/src/main/java/me/whiteship/demospring51/Demospring51Application.java
@@ -2,10 +2,8 @@ package me.whiteship.demospring51;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:/application.properties") //이 classpath에 들어있는 파일을 프로퍼티 소스에 놓겠다
 public class Demospring51Application {
 
     public static void main(String[] args) {
