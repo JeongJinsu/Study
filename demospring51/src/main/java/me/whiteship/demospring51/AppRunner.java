801c353a534edbfefdebd1ceba5ca33cfe0023fe
jin <jeongjinsu@outlook.com>
2020-10-06 16:14:07 +0900

    feat: 프로퍼티 / 설정방법 및 불러오는방법

--- demospring51/src/main/java/me/whiteship/demospring51/AppRunner.java
+++ demospring51/src/main/java/me/whiteship/demospring51/AppRunner.java
@@ -1,13 +1,12 @@
 package me.whiteship.demospring51;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
 
 @Component
 public class AppRunner implements ApplicationRunner {
@@ -18,10 +17,15 @@ public class AppRunner implements ApplicationRunner {
     @Autowired
     BookRepository bookRepository;
 
+    @Value("${app.name}") //스프링부트 에서 제공하는 프로퍼티 불러오는 방법
+    String appName;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         Environment environment = ctx.getEnvironment();
-        System.out.println(Arrays.toString(environment.getActiveProfiles()));
-        System.out.println(Arrays.toString(environment.getDefaultProfiles()));
+        System.out.println(environment.getProperty("app.name")); //설정-Edit-JVM으로 설정한 프로퍼티: -Dapp.name="spring" 를 불러오는 방법
+        System.out.println(environment.getProperty("app.about")); //.properties 확장 설정파일을 통해 설정한 프로퍼티 를 불러오는 방법
+
+        System.out.println(appName); //스프링부트 에서 제공하는 프로퍼티 불러오는 방법
     }
 }
