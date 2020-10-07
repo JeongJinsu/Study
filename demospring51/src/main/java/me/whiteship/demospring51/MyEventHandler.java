d308f81e1428699c0f6283693db4800bab4cf0ef
jin <jeongjinsu@outlook.com>
2020-10-07 16:05:09 +0900

    feat: 이벤트 핸들러에 @Order 애노테이션을 사용하여, 이벤트 처리 순서를 정하는 방법

--- demospring51/src/main/java/me/whiteship/demospring51/MyEventHandler.java
+++ demospring51/src/main/java/me/whiteship/demospring51/MyEventHandler.java
@@ -1,6 +1,8 @@
 package me.whiteship.demospring51;
 
 import org.springframework.context.ApplicationListener;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 //스프링 4.2버전 이전에는 ApplicationListener<해당 이벤트 타입 제네릭>를 반드시 상속받아서 이벤트 객체를 구현했어야 했다
@@ -8,11 +10,15 @@ import org.springframework.stereotype.Component;
 
 //핸들러는 빈으로 등록해야 한다
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE + 1) //핸들러가 여러개일때 우선순위 설정
 public class MyEventHandler implements ApplicationListener<MyEvent> {
 
     @Override //@EventListener (implements ApplicationListener<MyEvent>를 구현하지 않을땐 이 애노테이션을 이벤트 처리하는 메소드에 달아야 한다
     public void onApplicationEvent(MyEvent myEvent) {
+        System.out.println(Thread.currentThread().toString());
         System.out.println("이벤트를 받았다. 데이터는 " + myEvent.getData()); //이벤트를 받았다. 데이터는 100
         System.out.println(myEvent.getSource().toString()); //me.whiteship.demospring51.AppRunner@37b52340 //이벤트를 발생시킨 클래스
     }
+
+
 }
