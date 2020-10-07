782472c1168e64a8ab58c252814d0448b0fe74e9
jin <jeongjinsu@outlook.com>
2020-10-07 16:23:38 +0900

    feat: @Async 이벤트 비동기적 처리, 스프링부트가 제공하는 이벤트처리 방법(ContextRefreshedEvent, ContextClosedEvent)

--- demospring51/src/main/java/me/whiteship/demospring51/Demospring51Application.java
+++ demospring51/src/main/java/me/whiteship/demospring51/Demospring51Application.java
@@ -2,8 +2,10 @@ package me.whiteship.demospring51;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync //@Async 비동기적 이벤트 순서로 하고 싶을때 실행 클래스에 붙여야 함
 public class Demospring51Application {
 
     public static void main(String[] args) {
