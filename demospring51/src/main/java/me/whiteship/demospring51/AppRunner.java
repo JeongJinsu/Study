0ee21bc0cee2d3d5d4820995c7bfc6bc9b438873
jin <jeongjinsu@outlook.com>
2020-10-07 15:30:11 +0900

    feat: 이벤트 작성/발생/핸들러로 처리 하는 방법

--- demospring51/src/main/java/me/whiteship/demospring51/AppRunner.java
+++ demospring51/src/main/java/me/whiteship/demospring51/AppRunner.java
@@ -3,20 +3,18 @@ package me.whiteship.demospring51;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.MessageSource;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
-
-import java.util.Locale;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    MessageSource messageSource;
+    ApplicationContext applicationContext;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(messageSource.getMessage("greeting", new String[]{"keesun1"}, Locale.KOREA));
-        System.out.println(messageSource.getMessage("greeting", new String[]{"keesun2"}, Locale.getDefault())); //운영체제가 한글이라 _ko_KR.properties 적용됨
+        applicationContext.publishEvent(new MyEvent(this, 100)); //이벤트 발생 시킴
+
     }
 }
