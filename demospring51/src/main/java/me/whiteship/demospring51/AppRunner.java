d6dbd422df06a72d964fa9c385131a78f97da1e5
jin <jeongjinsu@outlook.com>
2020-10-08 16:11:26 +0900

    feat: 리소스 - java.net.URL을 추상화한 인터페이스 사용법 Resource 인터페이스의 구현체들(ClassPathResource, FileSystemResource, ServletContextResource)은 ApplicationContext의 타입에 따라 결정된다(ClassPathXmlApplicationContext, FileSystemXmlApplicationContext, WebApplicationContext)

--- demospring51/src/main/java/me/whiteship/demospring51/AppRunner.java
+++ demospring51/src/main/java/me/whiteship/demospring51/AppRunner.java
@@ -3,6 +3,7 @@ package me.whiteship.demospring51;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
@@ -14,11 +15,18 @@ import java.nio.file.Path;
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ResourceLoader resourceLoader;
+    ApplicationContext resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Resource resource = resourceLoader.getResource("classpath:/text.txt");
+        System.out.println(resourceLoader.getClass()); //AnnotationConfigServletWebServerApplicationContext
+
+        Resource resource = resourceLoader.getResource("classpath:/text.txt"); //classpath: 기준으로 리소스를 찾는다
+        System.out.println(resource.getClass()); //ClassPathResource
+
+        resource = resourceLoader.getResource("text.txt"); //웹 어플리케이션 루트(context path '') 상대경로 기준으로 리소스를 찾는다
+        System.out.println(resource.getClass()); //ServletContextResource
+
         System.out.println(resource.exists());
         System.out.println(resource.getDescription());
         System.out.println(Files.readString(Path.of(resource.getURI()))); //자바 11에서 추가된 ReadString 메소드
