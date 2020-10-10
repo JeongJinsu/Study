4084c732708635f9ac0d7bf717b3943403ac1127
jin <jeongjinsu@outlook.com>
2020-10-10 21:04:52 +0900

    feat: 고전적 데이터 바인딩을 개선한, Converter와 Formatter를 사용한 데이터바인딩

--- demospring51/src/main/java/me/whiteship/demospring51/EventController.java
+++ demospring51/src/main/java/me/whiteship/demospring51/EventController.java
@@ -9,15 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EventController {
 
-    /*
-     *Editor는 빈으로 사용하지 않고
-     *@InitBinder 어노테이션을 통해 컨트롤러에 데이터 바인더로서 지정등록할 수 있다
-     */
-    @InitBinder
-    public void init(WebDataBinder webDataBinder) {
-        webDataBinder.registerCustomEditor(Event.class, new EventEditor());
-    }
-
     @GetMapping("/event/{event}")
     public String getEvent(@PathVariable Event event) {
         System.out.println(event);
