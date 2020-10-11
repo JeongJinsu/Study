302281d213181097f16d80a6a5f6e70b8157e457
jin <jeongjinsu@outlook.com>
2020-10-11 17:40:41 +0900

    feat: SpEL의 기반 ExpressionParser

--- demospring51/src/main/java/me/whiteship/demospring51/AppRunner.java
+++ demospring51/src/main/java/me/whiteship/demospring51/AppRunner.java
@@ -3,6 +3,9 @@ package me.whiteship.demospring51;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -39,5 +42,19 @@ public class AppRunner implements ApplicationRunner {
         System.out.println(myValue);
         System.out.println(isMyValue100);
         System.out.println(sampleDate);
+
+
+        /*
+         * 스프링 EL의 기반 ExpressionParser
+         */
+        ExpressionParser parser = new SpelExpressionParser();
+        // .parseExpression 리턴값이 Expression 그 자체로 인자값 "#{2 + 100}" 과 동일
+        Expression expression = parser.parseExpression("2 + 100");
+        // Integer.class 타입으로 값을 가져오겠다 지정.
+        // (String -> Integer) 해당하는 타입으로 변환할때
+        // 데이터바인딩: ConversionService를 사용해 값을 변환후 가져온다
+        Integer value = expression.getValue(Integer.class);
+
+        System.out.println(value);
     }
 }
