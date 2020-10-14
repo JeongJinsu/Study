42f0a084097af5bbf446bb702d9309c9a7c4b070
jin <jeongjinsu@outlook.com>
2020-10-14 18:14:25 +0900

    feat: 스프링 AOP (@애노테이션 지정 방식)

--- demospring51/src/main/java/me/whiteship/demospring51/PerfAspect.java
+++ demospring51/src/main/java/me/whiteship/demospring51/PerfAspect.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 public class PerfAspect {
 
     //JoinPoint: @Around 타겟 메소드를 감싸는 형식으로
-    //PointCut: 경로 지정 ("...") 어디에 적용 하겠다
-    @Around("execution(* me.whiteship..*.EventService.*(..))")
+    //PointCut: @PerfLogging 애노테이션이 붙은 곳에 적용 하겠다
+    @Around("@annotation(PerfLogging)")
     public Object logPerf(ProceedingJoinPoint pjp) throws Throwable { //ProceedingJoinPoint: Advice가 적용되는 대상(Target 메소드)
         long begin = System.currentTimeMillis();
         Object reVal = pjp.proceed(); //Target 메소드(핵심 관심사) 실행
